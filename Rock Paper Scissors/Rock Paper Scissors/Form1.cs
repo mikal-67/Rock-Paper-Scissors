70a9c7764c5872b1a612673c874e484b9f5bdd01
mikal-67 <72887058+mikal-67@users.noreply.github.com>
2020-10-14 13:52:01 -0500

    Add files via upload

---
+++ Rock Paper Scissors/Rock Paper Scissors/Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Rock_Paper_Scissors
+{
+    public partial class Form1 : Form
+    {
+        int decision;
+        bool DecisionMade = false;
+        int comDecision;
+
+       
+        Random r = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+            comDecision = r.Next(1, 4);
+        }
+
+        private void button1_Click(object sender, EventArgs e)//Rock
+        {
+            decision = 1;
+            DecisionMade = true;
+            WinOrLose();
+            ButtonDisable();
+        }
+
+        private void button2_Click(object sender, EventArgs e)//Paper
+        {
+            decision = 2;
+            DecisionMade = true;
+            WinOrLose();
+           ButtonDisable();
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)//Scissors
+        { 
+            decision = 3;
+            DecisionMade = true;
+            WinOrLose();
+           ButtonDisable();
+        }
+        void ButtonDisable()
+        {
+            if(DecisionMade == true)
+            {
+                button1.Enabled = false;
+                button2.Enabled = false;
+                button3.Enabled = false;
+            }
+
+        }
+
+
+         void WinOrLose()
+         {
+            if (comDecision == decision)
+            {
+                textBox1.Text = "You tied.";
+            }
+            else if (comDecision == 1 && DecisionMade == true)//com is rock
+            {
+                if(decision == 3)
+                {
+                    textBox1.Text = "You lost...";
+                }
+                else if(decision == 2)
+                {
+                    textBox1.Text = "You Won!";
+                }               
+            }
+            else if (comDecision == 2 && DecisionMade == true)//com is paper
+            {
+                /*if (comDecision == 3 && decision == 1)
+                {
+                    textBox1.Text = "You Won!";
+                }
+                textBox1.Text = "You Won!";*/
+                if (decision == 1)
+                {
+                    textBox1.Text = "You lost...";
+                }
+                else if (decision == 3)
+                {
+                    textBox1.Text = "You Won!";
+                }
+            }
+            else if(comDecision == 3 && DecisionMade == true)//com is scis
+            {
+                // textBox1.Text = "You tied.";
+                if (decision == 2)
+                {
+                    textBox1.Text = "You lost...";
+                }
+                else if (decision == 1)
+                {
+                    textBox1.Text = "You Won!";
+                }
+            }
+            else
+            {
+                textBox1.Text = null;
+            }
+            
+
+
+            if (decision == 1)
+            {
+
+                textBox3.Text = "Your decision was Rock.";
+
+            }
+            else if (decision == 2)
+            {
+                textBox3.Text = "Your decision was Paper.";
+            }
+            else if (decision == 3)
+            {
+                textBox3.Text = "Your decision was Scissors.";
+            }
+
+            if (comDecision == 1)
+            {
+                textBox2.Text = "Your opponent's decision was Rock.";
+            }
+            else if (comDecision == 2)
+            {
+                textBox2.Text = "Your opponent's decision was Paper.";
+            }
+            else if (comDecision == 3)
+            {
+                textBox2.Text = "Your opponent's decision was Scissors.";
+            }
+        
+
+            button4.Enabled = true;
+
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)//reset vars
+        {
+            button1.Enabled = true;
+            button2.Enabled = true;
+            button3.Enabled = true;
+
+            comDecision = r.Next(1, 4);
+            decision = 0;
+            DecisionMade = false;
+            
+            button4.Enabled = false;
+
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+          
+        }
+    }
+}
---
+++ Rock Paper Scissors/Rock Paper Scissors/Properties/AssemblyInfo.cs
@@ -0,0 +1,38 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Rock Paper Scissors")]
+[assembly: AssemblyDescription("A fun game of rock-paper-scissors on your computer, featuring a replay button so you don't have to restart the game every time.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Rock Paper Scissors")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9c8da46e-b55d-42aa-8583-3134564702b9")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("en")]
